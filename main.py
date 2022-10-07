aa3b1c8a47ea02e6de8a20fabfd463fa987cf8db
PCDELMASTER\greedy <victorruizxz@gmail.com>
2022-10-07 18:24:22 +0200

    ...

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from os import path
 import io
 import sys
 
-user = "root"
-keypath = "/root/.ssh/root"
-remotehost = "192.168.58.49"
-defaultpassword = "root"
+user = "frodo"
+keypath = "/home/frodo/.ssh/frodo"
+remotehost = "192.168.1.134"
+defaultpassword = "bolson"
 ssh = SSHClient()
 ssh.set_missing_host_key_policy(AutoAddPolicy)
 
 print("Welcome to the ssh master")
-remotehost = input("Give your remote server IP (192.168.58.49): ") or remotehost
-defaultpassword = input("Give the default password (root): ") or defaultpassword
-user = input("Give the default username (root): ") or user
-keypath = input("Give the path for the key (/root/.ssh/root): ") or keypath
+remotehost = input("Give your remote server IP (192.168.1.134): ") or remotehost
+defaultpassword = input("Give the default password (bolson): ") or defaultpassword
+user = input("Give the default username (frodo): ") or user
+keypath = input("Give the path for the key (/home/frodo/.ssh/frodo): ") or keypath
 localpath = input("Give the path for the local file to copy (key path)") or keypath
 extpath = input("Give the path where you want to put the file (key path)") or keypath
 print("Will start the ssh connection")
@@ -32,7 +32,7 @@ else:
     print(i)
     if i == 0:
         sys.exit()
-print("hoalaaa")
+print("holaaa")
 if path.exists(localpath):
     print("holaaa")
     scp = SCPClient(ssh.get_transport())
